5b49ccfac8e2a9f3f5a4ec3fdf1c876cb4e35a9e
Rahil Mokashi <rahilmokashi07@gmail.com>
2025-07-17 22:05:32 +0530

    Made the code cleaner

--- turtle_random_walk/main.py
+++ turtle_random_walk/main.py
@@ -2,7 +2,15 @@ import turtle as t
 from turtle import Screen
 import random as ran
 
+
 t.colormode(255)
+
+def color():
+    r = ran.randint(0, 255)
+    g = ran.randint(0, 255)
+    b = ran.randint(0, 255)
+    color_combo = (r,g,b)
+    return color_combo
 
 direction = [0, 90, 180, 270]
 
@@ -12,10 +20,7 @@ tim.shape("classic")
 tim.width(7)
 tim.speed("fastest")
 for i in range(250):
-    r = ran.randint(0, 255)
-    g = ran.randint(0, 255)
-    b = ran.randint(0, 255)
-    tim.pencolor((r,g,b))
+    tim.pencolor(color())
     tim.setheading(ran.choice(direction))
     tim.forward(20)
 
